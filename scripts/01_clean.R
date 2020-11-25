16d5b21645fa14e35f553b0c552ca8979af25a64
emhedlin <hedlin@ualberta.ca>
2020-11-24 18:27:37 -0700

    verifying lay and hatchdates

--- scripts/01_clean.R
+++ scripts/01_clean.R
@@ -14,14 +14,41 @@ workbook_2014 <- xlsx_cells("data/sheet_2014.xlsx")
 
 
 lay_date <- workbook_2014 %>% 
-  filter(!is.na(date) & col %in% 3:6 & row %in% 6:10) 
+  filter(!is.na(date) & col %in% 3:6 & row %in% 6:10) %>%
+  select(site = sheet, laydate = date) %>%
+  mutate(site = str_replace(site, " ",  "")) %>%
+  mutate(site = str_replace(tolower(site), "site", "")) %>%
+  do({year(.$laydate)<-2014; .}) 
+
+
+lay_date <- lay_date %>% 
+  group_by(site)  %>% 
+  arrange(laydate, .by_group = TRUE) %>%
+  mutate(nestling_id = row_number())
+  
 
 
 # hatch date --------------------------------------------------------------
 
 
 hatch_date <- workbook_2014 %>% 
-  filter(!is.na(date) & col %in% 3:6 & row %in% 15:20) 
+  filter(!is.na(date) & col %in% 3:6 & row %in% 15:20) %>%
+  select(site = sheet, hatchdate = date) %>%
+  mutate(site = str_replace(site, " ",  "")) %>%
+  mutate(site = str_replace(tolower(site), "site", "")) %>%
+  do({year(.$hatchdate)<-2014; .}) 
+
+hatch_date <- hatch_date %>% 
+  group_by(site)  %>% 
+  arrange(hatchdate, .by_group = TRUE) %>%
+  mutate(nestling_id = row_number())
+  
+# calculate incubation to check for errors
+
+lay_date %>% left_join(hatch_date, by = c("site", "nestling_id")) %>%
+  mutate(incubation_time = as.double(hatchdate - laydate, units = "days")) %>% 
+  arrange(desc(incubation_time))
+
 
 # Chick weights -----------------------------------------------------------
 #WIP - eh
@@ -51,12 +78,24 @@ i = 2
 
 
 
-# Comment -----------------------------------------------------------------
+# comment -----------------------------------------------------------------
+occupied_sites <- c(100,119,12, 19, 20, 28,3, 
+                    33, 33,  35, 44, 47, 5,50,
+                    51,59,63,65,       
+                    67,72,75,76,77,78,       
+                    79,8,85,88,95,97)
 
 # various info to be pulled from comment cell
 
 # this is going to take some creativity...
-workbook_2014 %>% 
+x <- workbook_2014 %>% 
   filter(col %in% 18 & row %in% 15) %>%
   select(sheet, character) %>%
-  filter(sheet != "Ex" & !is.na(character))
+  filter(sheet != Ex,& !is.na(character))
+
+# string length should be indicative of occupancy, but is it reliable
+x %>% filter(str_length(character) > 500)
+x %>% filter(str_detect(character, "PEFA"))
+x %>% filter(str_detect(character, "RLHA"))
+x %>% filter(str_detect(character, "nestling") | str_detect(character, "chicks"))
+x %>% filter(str_detect(character, "quail"))
